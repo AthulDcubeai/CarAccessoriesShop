6d9f05f5dd65852cb794ce0e3e642cb8ccfef31f
Athul S <athul.s@dcube.local>
2024-09-05 16:34:32 +0530

    Initial commit

---
+++ CarAccessoriesShop/Models/Invoice.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace CarAccessoriesShop.Models
+{
+    public class Invoice
+    {
+    public int InvoiceId { get; set; }
+    public int OrderId { get; set; }
+    public DateTime InvoiceDate { get; set; }
+    public decimal TotalAmount { get; set; }
+    public virtual Order Order { get; set; }
+    }
+}
---
+++ CarAccessoriesShop/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarAccessoriesShop.Models
+{
+    public class Order
+    {
+    public int OrderId { get; set; }
+    public int CustomerId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public virtual Customer Customer { get; set; }
+    public virtual ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
